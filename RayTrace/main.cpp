7537435ae9b3a65fa9cb9e8f596eff54cccc3dcd
onur <onurtaskiran95@gmail.com>
2020-11-25 21:34:58 +0300

    First image output

---
+++ RayTrace/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    // Image 
+    const int image_width = 256;
+    const int image_height = 256;
+
+    // PPM file format
+    // P3       -> means this is a RGB color image in ASCII
+    // 256 256  -> width & height of the image
+    // 255      -> maximum value for each color
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    // Image data
+    for (int j = image_height -1; j >= 0; j--) // Columns -> top to bottom
+    {
+        for (int i = 0; i < image_width; i++) // Columns -> left to right
+        {
+            int r = i; // Red channel is increase to the right
+            int g = j; // Green channel is decreasing to the bottom
+            int b = (i+j)/2; // Blue channel averages Red and Green channel in each pixel
+
+            std::cout << r << ' ' << g << ' ' << b << '\n'; // -> Pixel output
+        }
+    }
+}
