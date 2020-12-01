e59458c7dfd11fe7b6ce768d99756faaf3260c02
onur <onurtaskiran95@gmail.com>
2020-12-01 07:05:53 +0300

    Add operator overloading to vec3 class

--- RayTrace/vec3.h
+++ RayTrace/vec3.h
@@ -1,5 +1,7 @@
 #pragma once VEC3_H
 
+#include <iostream>
+
 struct vec3
 {
 	double v[3];
@@ -15,3 +17,35 @@ struct vec3
 	double z() const { return v[2]; };
 
 };
+
+vec3 operator+(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v[0] + v2.v[0], v1.v[1] + v2.v[1], v1.v[2] + v2.v[2]);
+}
+
+vec3 operator-(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v[0] - v2.v[0], v1.v[1] - v2.v[1], v1.v[2] - v2.v[2]);
+}
+
+vec3 operator/(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v[0] / v2.v[0], v1.v[1] / v2.v[1], v1.v[2] / v2.v[2]);
+}
+
+vec3 operator/(const vec3& v1, double& t)
+{
+	return vec3(v1.v[0] / t, v1.v[1] / t, v1.v[2] / t);
+}
+
+vec3 operator*(const vec3& v1, const vec3& v2)
+{
+	return vec3(v1.v[0] * v2.v[0], v1.v[1] * v2.v[1], v1.v[2] * v2.v[2]);
+}
+
+vec3 operator*(const vec3& v1, double& t)
+{
+	return vec3(v1.v[0] * t, v1.v[1] * t, v1.v[2] * t);
+}
+
+
