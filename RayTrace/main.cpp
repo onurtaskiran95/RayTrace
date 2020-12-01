6b91eb768ef2416fffe33dcb9fe88d01f7322c09
onur <onurtaskiran95@gmail.com>
2020-12-01 03:37:50 +0300

    Edit vec3 class

--- RayTrace/main.cpp
+++ RayTrace/main.cpp
@@ -5,26 +5,30 @@
 
 int main()
 {
-    // Image 
-    const int image_width = 256;
-    const int image_height = 256;
+    //// Image 
+    //const int image_width = 256;
+    //const int image_height = 256;
 
-    // PPM file format
-    // P3       -> means this is a RGB color image in ASCII
-    // 256 256  -> width & height of the image
-    // 255      -> maximum value for each color
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    //// PPM file format
+    //// P3       -> means this is a RGB color image in ASCII
+    //// 256 256  -> width & height of the image
+    //// 255      -> maximum value for each color
+    //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    // Image data
-    for (int j = image_height -1; j >= 0; j--) // Columns -> top to bottom
-    {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) // Columns -> left to right
-        {
-            color pixel_color(double(i) / (image_width-1), double(j) / (image_height-1), double(i+j)/(image_height + image_width -2));
-            write_color(std::cout, pixel_color);
-        }
-    }
+    //// Image data
+    //for (int j = image_height - 1; j >= 0; j--) // Columns -> top to bottom
+    //{
+    //    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+    //    for (int i = 0; i < image_width; i++) // Columns -> left to right
+    //    {
+    //        color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), double(i + j) / (image_height + image_width - 2));
+    //        write_color(std::cout, pixel_color);
+    //    }
+    //}
 
-    std::cerr << "\nDone..\n";
+    //std::cerr << "\nDone..\n";
+
+
+
+    std::cin.get();
 }
